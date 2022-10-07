9431030ff53e9f33376310c11be445c2398ee94e
Apz <793469695@qq.com>
2022-10-08 03:08:11 +0800

    first commit

---
+++ src/main/java/com/wz/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wz.git;
+
+/**
+ * @author --|-
+ * @create 2022-10-08-3:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git....");
+    }
+}
