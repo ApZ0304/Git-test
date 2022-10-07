4a5261d18300e42c5f6347b3ea7e22921f1efb83
Apz <793469695@qq.com>
2022-10-08 03:09:35 +0800

    second commit

--- src/main/java/com/wz/git/GitTest.java
+++ src/main/java/com/wz/git/GitTest.java
@@ -7,5 +7,6 @@ package com.wz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git....");
+        System.out.println("hello git22222222222....");
     }
 }
