1cb52c45cc2e39489795a44501de2f715b4b2d49
Apz <793469695@qq.com>
2022-10-08 03:15:37 +0800

    hot-fix commit1

--- src/main/java/com/wz/git/GitTest.java
+++ src/main/java/com/wz/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git....");
         System.out.println("hello git22222222222....");
+        System.out.println("hello git---Hot-Fix....");
     }
 }
